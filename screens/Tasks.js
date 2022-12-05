2ae7da6b2e2fc51749bdc0d3f068b43a3aa20fd3
Ahmed Abozaid <ahmedabozaid493@gmail.com>
2022-12-05 12:01:45 +0200

    adds the screen component to make sure the app doesn't reach the status bar

---
+++ screens/Tasks.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+import Screen from "../components/Screen";
+
+export default function Tasks() {
+  return (
+    <Screen>
+      <Text>Tasks</Text>
+    </Screen>
+  );
+}
